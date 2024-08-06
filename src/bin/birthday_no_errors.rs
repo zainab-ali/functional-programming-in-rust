32bb3a1dce0d0156726101f4e1d832fe61d59df0
zainab-ali <zainab@kebab-ca.se>
2024-08-06 22:07:38 +0100

    Add no errors.

--- src/bin/birthday_no_errors.rs
+++ src/bin/birthday_no_errors.rs
@@ -1,20 +1,64 @@
-use std::fmt::Write;
-
 use chrono::{Datelike, NaiveDate, Utc};
 use Message::*;
 
+// We now no longer panic on invalid input. Instead, we exit with a more informative code.
 fn main() -> Result<(), Error> {
-    let today = today();
-    let date_string = std::env::args().nth(1);
-    let message = calc_message(date_string, today)?;
+    // Side effects
+    let today = get_today();
+    let input = get_arg();
+
+    // Pure code
+    let message = calc_message(input, today)?;
+
+    // Side effects
     print_message(message);
     Ok(())
 }
-#[derive(Debug, PartialEq)]
-enum Error {
-    NoDateOfBirth,
-    BadDateOfBirth(String),
-    BornInTheFuture,
+
+fn get_today() -> NaiveDate {
+    Utc::now().date_naive()
+}
+
+fn get_arg() -> Option<String> {
+    std::env::args().nth(1)
+}
+
+fn calc_message(input: Option<String>, today: NaiveDate) -> Result<Message, Error> {
+    let dob = calc_dob(input)?;
+    let birthday = calc_birthday(today, dob)?;
+    let message = if birthday == today {
+        HappyBirthday {
+            age: calc_age(today, dob),
+        }
+    } else if birthday < today {
+        HadBirthday
+    } else {
+        Wait {
+            days: calc_days_until_birthday(today, birthday) as u32,
+        }
+    };
+    Ok(message)
+}
+
+fn calc_dob(input: Option<String>) -> Result<NaiveDate, Error> {
+    let date_string = input.ok_or(Error::NoDateOfBirth)?;
+    NaiveDate::parse_from_str(&date_string, "%Y-%m-%d").map_err(|_| Error::BadDateOfBirth(date_string))
+}
+
+fn calc_birthday(today: NaiveDate, dob: NaiveDate) -> Result<NaiveDate, Error> {
+    if dob <= today {
+        // For simplicity, we don't handle leap years.
+        Ok(dob.with_year(today.year()).unwrap())
+    } else {
+        Err(Error::BornInTheFuture)
+    }
+}
+
+fn calc_days_until_birthday(today: NaiveDate, birthday: NaiveDate) -> i64 {
+    birthday.signed_duration_since(today).num_days()
+}
+fn calc_age(today: NaiveDate, dob: NaiveDate) -> u32 {
+    today.years_since(dob).unwrap()
 }
 
 #[derive(Debug, PartialEq)]
@@ -22,7 +66,13 @@ enum Message {
     HappyBirthday { age: u32 },
     HadBirthday,
     Wait { days: u32 },
-    NoBirthday,
+}
+
+#[derive(Debug, PartialEq)]
+enum Error {
+    NoDateOfBirth,
+    BadDateOfBirth(String),
+    BornInTheFuture,
 }
 
 fn print_message(message: Message) -> () {
@@ -30,78 +80,31 @@ fn print_message(message: Message) -> () {
         HappyBirthday { age } => println!("Happy birthday! Congratulations on becoming {}!", age),
         HadBirthday => println!("You've already had your birthday. I hope you had fun!"),
         Wait { days } => println!("It's not your birthday yet. Wait for {} more days.", days),
-        NoBirthday => println!("Sadly no birthday for you this year."),
     }
-}
-
-fn calc_message(dob_string: Option<String>, today: NaiveDate) -> Result<Message, Error> {
-    let dob = calc_dob(dob_string, today)?;
-    let message = match get_birthday(today, dob) {
-        None => NoBirthday,
-        Some(birthday) => {
-            if birthday == today {
-                Message::HappyBirthday {
-                    age: age(today, dob),
-                }
-            } else {
-                if birthday < today {
-                    Message::HadBirthday
-                } else {
-                    Message::Wait {
-                        days: days_until_birthday(today, birthday) as u32,
-                    }
-                }
-            }
-        }
-    };
-    Ok(message)
-}
-
-fn days_until_birthday(today: NaiveDate, birthday: NaiveDate) -> i64 {
-    birthday.signed_duration_since(today).num_days()
-}
-fn age(today: NaiveDate, date_of_birth: NaiveDate) -> u32 {
-    // The dob is after today.
-    today.years_since(date_of_birth).unwrap()
-}
-fn calc_dob(date_string: Option<String>, today: NaiveDate) -> Result<NaiveDate, Error> {
-    // There is no argument specified
-    match date_string {
-        None => Err(Error::NoDateOfBirth),
-        Some(str) => NaiveDate::parse_from_str(&str, "%Y-%m-%d")
-            .map_err(|_| Error::BadDateOfBirth(str))
-            .and_then(|date| {
-                if date <= today {
-                    Ok(date)
-                } else {
-                    Err(Error::BornInTheFuture)
-                }
-            }),
-    }
-}
-fn get_birthday(today: NaiveDate, date_of_birth: NaiveDate) -> Option<NaiveDate> {
-    // This will fail if the user was born on a leap year
-    date_of_birth.with_year(today.year())
-}
-
-fn today() -> NaiveDate {
-    Utc::now().date_naive()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn born_in_future() {
-        let today = NaiveDate::from_ymd(2025, 01, 01);
-        let result = calc_message(Some("2030-06-15".to_string()), today);
-        assert_eq!(result, Err(Error::BornInTheFuture));
+    fn test_days_until_birthday() {
+        let today = NaiveDate::from_ymd(2024, 08, 05);
+        let birthday = NaiveDate::from_ymd(2024, 08, 06);
+        let result = calc_days_until_birthday(today, birthday);
+        assert_eq!(result, 1);
+    }
+    
+    #[test]
+    fn test_had_birthday() {
+        let today = NaiveDate::from_ymd(2024, 08, 06);
+        let result = calc_message(Some("2000-08-05".to_string()), today);
+        assert_eq!(result, Ok(HadBirthday));
     }
 
     #[test]
-    fn born_on_a_leap_year() {
-        let today = NaiveDate::from_ymd(2025, 01, 01);
-        let result = calc_message(Some("2000-02-29".to_string()), today);
-        assert_eq!(result, Ok(Message::HadBirthday));
+    fn test_born_in_future() {
+        let today = NaiveDate::from_ymd(2024, 08, 06);
+        let result = calc_message(Some("2030-08-05".to_string()), today);
+        assert_eq!(result, Err(Error::BornInTheFuture));
     }
 }
